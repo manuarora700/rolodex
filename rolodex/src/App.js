d311f18a83f77f788b4274da392c3c94b3f0ada2
Manu Arora <mannuarora700@gmail.com>
2020-08-24 19:14:55 +0530

    Fetch users

--- rolodex/src/App.js
+++ rolodex/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -7,20 +6,20 @@ class App extends Component {
     super();
 
     this.state = {
-      string: "Hello Paaji",
+      monsters: [],
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: "Hello Arora ji" })}>
-            {" "}
-            Change Text{" "}
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
