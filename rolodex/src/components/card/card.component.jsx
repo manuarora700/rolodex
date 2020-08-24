f0e84098b08579679f2990fc19ee9bb9e15c9a09
Manu Arora <mannuarora700@gmail.com>
2020-08-24 19:44:50 +0530

    Images from api

---
+++ rolodex/src/components/card/card.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./card.styles.css";
+
+export const Card = (props) => {
+  return (
+    <div className="card-container">
+      <img
+        alt="monster"
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x200`}
+      />
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
+    </div>
+  );
+};
