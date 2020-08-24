e5462287e3f5540d6eee1eff0668768347a82a60
Manu Arora <mannuarora700@gmail.com>
2020-08-24 19:31:51 +0530

    Basic styling

--- rolodex/src/App.js
+++ rolodex/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import { CardList } from "./components/card-list/card-list.component";
+
 class App extends Component {
   constructor() {
     super();
@@ -17,9 +19,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList name="Paaji">
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
